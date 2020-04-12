90ab46f7faa77bb879ed5f29eae314439cc2a522
sali <sali@loyalty.com>
2020-04-12 15:06:24 -0400

    updated

--- common/src/main/scala/com/alphasystem/game/uno/model/response/ResponseEnvelope.scala
+++ common/src/main/scala/com/alphasystem/game/uno/model/response/ResponseEnvelope.scala
@@ -1,13 +1,14 @@
 package com.alphasystem.game.uno.model.response
 
-import io.circe.{Decoder, Encoder}
+import io.circe._
+import io.circe.generic.auto._
 
 case class ResponseEnvelope(playerId: Int, responseType: ResponseType, payload: ResponsePayload)
 
 object ResponseEnvelope {
-  implicit val MessageEnvelopeDecoder: Decoder[ResponseEnvelope] =
+  implicit val ResponseEnvelopeDecoder: Decoder[ResponseEnvelope] =
     Decoder.forProduct3("player-id", "type", "payload")(ResponseEnvelope.apply)
 
-  implicit val MessageEnvelopeEncoder: Encoder[ResponseEnvelope] =
+  implicit val ResponseEnvelopeEncoder: Encoder[ResponseEnvelope] =
     Encoder.forProduct3("player-id", "type", "payload")(me => (me.playerId, me.responseType, me.payload))
 }
--- common/src/main/scala/com/alphasystem/game/uno/model/response/ResponseType.scala
+++ common/src/main/scala/com/alphasystem/game/uno/model/response/ResponseType.scala
@@ -1,10 +1,13 @@
 package com.alphasystem.game.uno.model.response
 
-import enumeratum.{CirceEnum, Enum, EnumEntry}
 import enumeratum.EnumEntry.Hyphencase
+import enumeratum.{CirceEnum, Enum, EnumEntry}
 
 sealed trait ResponseType extends EnumEntry with Hyphencase
 
 object ResponseType extends Enum[ResponseType] with CirceEnum[ResponseType] {
   override def values: IndexedSeq[ResponseType] = findValues
+
+  final case object None extends ResponseType
+
 }
