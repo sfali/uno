1eed284c67f57ac58fd43b0dd580d1b644e8e70f
sali <sali@loyalty.com>
2020-04-16 12:08:26 -0400

    updated

--- common/src/main/scala/com/alphasystem/game/uno/model/response/MessageCode.scala
+++ common/src/main/scala/com/alphasystem/game/uno/model/response/MessageCode.scala
@@ -2,7 +2,7 @@ package com.alphasystem.game.uno.model.response
 
 import enumeratum.{CirceEnum, Enum, EnumEntry}
 
-sealed trait MessageCode extends EnumEntry with EnumEntry.Hyphencase
+sealed trait MessageCode extends EnumEntry
 
 object MessageCode extends Enum[MessageCode] with CirceEnum[MessageCode] {
   override def values: IndexedSeq[MessageCode] = findValues
@@ -12,4 +12,5 @@ object MessageCode extends Enum[MessageCode] with CirceEnum[MessageCode] {
   final case object IllegalMove extends MessageCode
 
   final case object IllegalAccess extends MessageCode
+
 }
