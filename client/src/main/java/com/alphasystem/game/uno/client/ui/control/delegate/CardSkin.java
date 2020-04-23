9453e0ccc0d1838332a11c162ee64ec1f1ea0d95
sali <sali@loyalty.com>
2020-04-23 10:15:01 -0400

    need to set image at the load and use of VBox

--- client/src/main/java/com/alphasystem/game/uno/client/ui/control/delegate/CardSkin.java
+++ client/src/main/java/com/alphasystem/game/uno/client/ui/control/delegate/CardSkin.java
@@ -1,13 +1,12 @@
 package com.alphasystem.game.uno.client.ui.control.delegate;
 
 import com.alphasystem.game.uno.model.Card;
-import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.SkinBase;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 import scala.Tuple2;
@@ -20,7 +19,7 @@ class CardSkin extends SkinBase<CardView> {
 
     private static final Image BLANK_IMAGE = cropImage(BASE_IMAGE, 0, 1440);
 
-    private final GridPane pane = new GridPane();
+    private final VBox pane = new VBox();
     private final ImageView image = new ImageView(BLANK_IMAGE);
     private final Label label = new Label();
 
@@ -32,7 +31,7 @@ class CardSkin extends SkinBase<CardView> {
         image.setOnMouseClicked(event -> control.setSelected(!control.isSelected()));
         control.fitHeightProperty().addListener((observable, oldValue, newValue) -> {
             Integer height = (Integer) newValue;
-            if (height == null || height < 128) {
+            if (height == null) {
                 height = 128;
             }
             image.setFitHeight(height);
@@ -40,7 +39,8 @@ class CardSkin extends SkinBase<CardView> {
         control.playerNameProperty().addListener((observable, oldValue, newValue) -> setPlayerName(newValue));
         image.setPreserveRatio(true);
 
-        pane.add(image, 0, 0);
+        setImage(getSkinnable().getCard());
+        pane.getChildren().add(image);
         setPlayerName(control.getPlayerName());
 
         getChildren().addAll(pane);
@@ -49,9 +49,12 @@ class CardSkin extends SkinBase<CardView> {
     private void setImage(Card newValue) {
         if (newValue == null) {
             image.setImage(BLANK_IMAGE);
+            image.setVisible(false);
+            image.setUserData(null);
         } else {
             final Tuple2<Object, Object> coordinates = newValue.toImageCoordinates();
             final Image value = cropImage(BASE_IMAGE, (Integer) coordinates._1(), (Integer) coordinates._2());
+            image.setVisible(true);
             image.setImage(value);
             image.setUserData(newValue);
         }
@@ -64,9 +67,8 @@ class CardSkin extends SkinBase<CardView> {
         } else {
             label.setText(playerName);
             label.setTextAlignment(TextAlignment.CENTER);
-            label.setFont(Font.font("Georgia", 24));
-            GridPane.setHalignment(label, HPos.CENTER);
-            pane.add(label, 0, 1);
+            label.setFont(Font.font(Util.SYSTEM_FONT_NAME, 24));
+            pane.getChildren().add(label);
         }
     }
 
