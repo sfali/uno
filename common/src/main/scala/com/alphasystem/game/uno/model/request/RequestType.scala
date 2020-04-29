486820d3dcf1ba34be0bac9fe10e0a6dbc3ea8df
sali <sali@loyalty.com>
2020-04-29 13:13:50 -0400

    request types for Fail and game ended (required for client)

--- common/src/main/scala/com/alphasystem/game/uno/model/request/RequestType.scala
+++ common/src/main/scala/com/alphasystem/game/uno/model/request/RequestType.scala
@@ -8,6 +8,8 @@ sealed trait RequestType extends EnumEntry with Hyphencase
 object RequestType extends Enum[RequestType] with CirceEnum[RequestType] {
   override def values: IndexedSeq[RequestType] = findValues
 
+  final case object Fail extends RequestType
+
   /**
    * Command to start a game
    */
@@ -16,4 +18,6 @@ object RequestType extends Enum[RequestType] with CirceEnum[RequestType] {
   final case object StartGameApproved extends RequestType
 
   final case object StartGameRejected extends RequestType
+
+  final case object GameEnded extends RequestType
 }
